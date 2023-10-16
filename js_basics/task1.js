035afb7d82ccfab36f8bcb282e39ed72b7721f73
KaterynaMyronenko <miironenko@gmail.com>
2023-10-16 23:37:16 +0300

    homework-js-basics

---
+++ js_basics/task1.js
@@ -0,0 +1,6 @@
+let num = 15
+let text = 'Hello, World!'
+let isTrue = true
+let value = null
+let anotherValue
+console.log(num, text, isTrue, value, anotherValue)
---
+++ js_basics/task5.1.js
@@ -0,0 +1,3 @@
+let radius = 7
+let area = (Math.PI * Math.pow(radius, 2)).toFixed(2)
+console.log(area)
